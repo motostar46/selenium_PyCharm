372496eeb7051b0428a5eee4b7b8e483c25b9f29
Oleg <48244223+motostar46@users.noreply.github.com>
2019-03-06 21:38:45 +0300

    First task

--- first.py
+++ first.py
@@ -1,1 +1,21 @@
-print("Hello123")
+import unittest
+from selenium import webdriver
+
+
+class SearchTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+        cls.driver.implicitly_wait(30)
+        cls.driver.maximize_window()
+
+        cls.driver.get("http://software-testing.ru/")
+
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
