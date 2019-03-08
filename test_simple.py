5db2b3bf9ad6235166e6dc1db93a161aa9613227
Oleg <48244223+motostar46@users.noreply.github.com>
2019-03-08 18:46:38 +0300

    Edited simple test case

--- test_simple.py
+++ test_simple.py
@@ -1,7 +1,5 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.fixture
