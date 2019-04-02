09a5421acdd8d43c7d61c49c94c77a75e374b026
oleg <motostar46@mail.ru>
2019-04-02 19:24:45 +0300

    17 task changes

--- task_17.py
+++ task_17.py
@@ -10,6 +10,7 @@ def driver(request):
     options = webdriver.ChromeOptions()
     options.binary_location = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
     options.add_argument("start-maximized")
+    options.set_capability('loggingPrefs', {'browser':'ALL' })
 
     wd = webdriver.Chrome(chrome_options=options)
     wd.implicitly_wait(3)
@@ -24,9 +25,10 @@ def test_logs(driver):
     for item in range(len(all_items)):
         all_items = driver.find_elements_by_css_selector("[href*=product][title=Edit]")
         all_items[item].click()
-        if driver.get_log("browser"):
-            for l in driver.get_log("browser"):
-                print("here is logs: ", l)
+        logs = driver.get_log("browser")
+        if logs:
+            for l in logs:
+                print("\nhere is logs: ", l)
         else:
-            print("There are no errors")
+            print("There are no errors \n")
         driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1")
